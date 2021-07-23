770904bcfda8ba077c13cb731550ed53817f15ae
PrachiPhatak <phatak.prachi@gmail.com>
2021-07-23 10:23:49 -0500

    Add test case

--- src/test/java/CountryTest.java
+++ src/test/java/CountryTest.java
@@ -1,6 +1,4 @@
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.*;
 
@@ -10,13 +8,30 @@ public class CountryTest {
 
     @Before
     public void setUp(){
+        System.out.println("In setup");
         country = new Country("USA", "Washington DC", 4.2);
     }
 
     @Test
     public void testConstructor(){
+        System.out.println("In testConstructor");
         assertEquals("USA", country.name);
         assertEquals("Washington DC", country.capital);
         assertEquals(4.2, country.worldShare, 0);
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass(){
+        System.out.println("Before class");
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass(){
+        System.out.println("After class");
+    }
+
+    @After
+    public void tearDown(){
+        System.out.println("After");
     }
 }
